76f3b1271ba94612264286da80ce8a9d69f91fde
Kausara Kpabia <kausara931@gmail.com>
2020-05-29 11:28:01 -0700

    This is a fresh commit!

---
+++ exercise.py
@@ -0,0 +1,20 @@
+""""
+#Exercise 1-Hello
+name = input("What is your name")
+print('hello', name)
+"""
+
+"""
+#Exercise 2- Area of a room
+width = float(input('What is the width of your room'))
+length = float(input('What is its length'))
+print('The area of your room is :', str(width * length) , 'fts')
+"""
+
+"""
+#Exercise 3- Area of a field
+width = float(input('What is the width of your farm in fts'))
+length = float(input('What is its length in fts'))
+area = (width * length) / 43560
+print('The area of your field is :' , str(area) , 'acres' )
+"""
